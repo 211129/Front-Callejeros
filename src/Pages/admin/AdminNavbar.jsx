3d682113142a24730d3f92da78e3faa2dd3bcd02
yumari211225 <211225@ids.upchiapas.edu.mx>
2022-12-12 10:03:07 -0600

    Admin navbar

--- src/Pages/admin/AdminNavbar.jsx
+++ src/Pages/admin/AdminNavbar.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 
 function AdminNavbar() {
     return (
-        <nav className="navbar navbar-light bg-light justify-content-between p-4">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between p-4">
             <a className="navbar-brand">Callejeros Admin</a>
             <ul className='navbar-nav mr-auto'>
                 <li class="nav-item active">
                     <a class="nav-link" ><Link to="/admin" className='form_link'>Admin Dashboard</Link><span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" ><Link to="/" className='form_link'>Tienda</Link></a>
                 </li>
             </ul>
             <form className="form-inline">
